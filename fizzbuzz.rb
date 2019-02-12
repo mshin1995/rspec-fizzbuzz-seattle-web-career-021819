570f41a290a08bb6b2c5d085b825fa729e9fb5e1
Matthew Shin <mshin1995@gmail.com>
2019-02-12 01:38:28 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,7 +1,12 @@
 def fizzbuzz(int)
   if int % 3 === 0 
     "Fizz"
-  elsif int % 3 === 0 
-    "Bu"
+  elsif int % 5 === 0 
+    "Buzz"
+  elsif int % (3 && 5) === 0 
+    "FizzBuzz"
+  else
+    nil
+  end
   
 end
